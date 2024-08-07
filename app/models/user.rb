d863f803800491d0cb2fea3894d6a8e8cdda00d3
ryan horowitz <ryan.w.horowitz@gmail.com>
2024-08-07 15:21:39 -0400

    added more email validation to the user model. Makes sure the email fits the normal email123@mail.com format

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
   has_secure_password
 
-  validates :email, presence: true, uniqueness: true
+  validates :email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP }
 end
