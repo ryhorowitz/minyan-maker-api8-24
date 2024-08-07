915158977c11e3326daa6771813f0e057abf572b
ryan horowitz <ryan.w.horowitz@gmail.com>
2024-08-07 15:08:43 -0400

    added cors to config/application to allow requests.

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,6 @@
-require_relative "boot"
+require_relative 'boot'
 
-require "rails/all"
+require 'rails/all'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -23,5 +23,15 @@ module MinyanMakerApi
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    # CORS setup
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins '*' # Update this with your front-end URL for production
+        resource '*',
+                 headers: :any,
+                 methods: %i[get post put patch delete options head]
+      end
+    end
   end
 end
