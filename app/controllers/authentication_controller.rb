8247e5376e2b75ac44b7919ca529666cc9a136b1
ryan horowitz <ryan.w.horowitz@gmail.com>
2024-08-07 15:15:48 -0400

    added routes for authentication controller. Built out Authenticationcontroller methods.

--- app/controllers/authentication_controller.rb
+++ app/controllers/authentication_controller.rb
@@ -1,7 +1,43 @@
 class AuthenticationController < ApplicationController
+  before_action :authorize_request, except: :login
+
+  # POST /signup
   def signup
+    user = User.new(user_params)
+    if user.save
+      render json: user, status: :created
+    else
+      render json: { errors: user.errors.full_messages }, status: :unprocessable_entity
+    end
   end
 
+  # POST /login
   def login
+    user = User.find_by(email: params[:email])
+    if user&.authenticate(params[:password])
+      token = encode_token(user_id: user.id)
+      render json: { token: token, user: user }, status: :ok
+    else
+      render json: { errors: 'Invalid email or password' }, status: :unauthorized
+    end
+  end
+
+  private
+
+  def user_params
+    params.permit(:email, :password)
+  end
+
+  def encode_token(payload)
+    JWT.encode(payload, 'your_secret_key')
+  end
+
+  def authorize_request
+    header = request.headers['Authorization']
+    header = header.split(' ').last if header
+    decoded = JWT.decode(header, 'your_secret_key')[0]
+    @current_user = User.find(decoded['user_id'])
+  rescue ActiveRecord::RecordNotFound, JWT::DecodeError
+    render json: { errors: 'Unauthorized' }, status: :unauthorized
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  get 'authentication/signup'
-  get 'authentication/login'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  post '/signup', to: 'authentication#signup'
+  post '/login', to: 'authentication#login'
 end
